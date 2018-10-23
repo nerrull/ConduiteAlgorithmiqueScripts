346cb0e4eed0460dd4382c588d4a66f19af97351
nerrull <et.richan@gmail.com>
2018-10-23 14:34:01 -0400

    bugfixes

--- convertToHAP.py
+++ convertToHAP.py
@@ -4,6 +4,22 @@ from util import get_video_start
 from decimal import *
 dir = "/home/nuc/Documents/dataStore/VIDEO"
 
+
+def convert(infile, outfile):
+
+    print ("Encoding file {} - {}/{}".format( file, i+1, numfiles))
+    start = get_video_start(infile)
+    split_cmd = "ffmpeg -i {}  -ss 00:00:0{} -c:v hap -chunks 3 -c:a aac -strict -2 -async 1 {}   ".format(infile,Decimal(start),  outfile)
+    subprocess.call(split_cmd, shell=True)
+
+
+def convert_resize(infile, outfile):
+
+    print ("Encoding file {} - {}/{}".format( file, i+1, numfiles))
+    start = get_video_start(infile)
+    split_cmd = "ffmpeg -y -i {} -vf scale=w=1920:h=1080 -ss 00:00:0{} -c:v hap -chunks 3 -c:a aac -strict -2 -async 1 {}   ".format(infile,Decimal(start),  outfile)
+    subprocess.call(split_cmd, shell=True)
+
 files = [file for file in os.listdir(dir) if file.upper().endswith(".MP4")]
 numfiles =len(files)
 for i,file in enumerate(files):
@@ -13,7 +29,5 @@ for i,file in enumerate(files):
     if os.path.exists(outfile):
         continue
 
-    print ("Encoding file {} - {}/{}".format( file, i+1, numfiles))
-    start = get_video_start(infile)
-    split_cmd = "ffmpeg -i {}  -ss 00:00:0{} -c:v hap  -c:a aac -strict -2 -async 1 {}   ".format(infile,Decimal(start),  outfile)
-    subprocess.call(split_cmd, shell=True)
+    convert(infile, outfile)
+
--- resizeOverSizeVideos.py
+++ resizeOverSizeVideos.py
@@ -1,7 +1,8 @@
 import os
 import subprocess
 from util import get_video_start
-
+from os.path import basename
+from convertToHAP import convert_resize
 
 known_files = [
     "VIRB_0280_000004.mp4",
@@ -14,7 +15,6 @@ known_files = [
     "VIRB_0280_000354.mp4"
 ]
 
-dir = VIDEO_DIR
 
 def find_oversize(dir):
     files = [file for file in os.listdir(dir) if file.endswith(".mp4")]
@@ -30,18 +30,18 @@ def find_oversize(dir):
         output = subprocess.Popen(check_cmd,
                                   shell=True,
                                   stdout=subprocess.PIPE
-                                  ).stdout.read()
+                                  ).stdout.read().decode("utf-8")
         if "1920" not in output:
             print ("Found file {} - {}/{}".format(file, i + 1, numfiles))
             # print (output)
-            filelist.append(basename(filename))
+            filelist.append(file)
 
         if (i %100) ==0:
-            print ("Progress {}/{}").format(i+1, numfiles)
+            print ("Progress {}/{}".format(i+1, numfiles))
 
     return filelist
 
-def reencode_oversize(files):
+def reencode_oversize(dir,files):
     numfiles = len(files)
     for i, file in enumerate(files):
         print ("Checking file {} - {}/{}".format(file, i + 1, numfiles))
@@ -49,16 +49,11 @@ def reencode_oversize(files):
         infile = os.path.join(dir, file)
         # outfile = os.path.join(dir, filename + "_resize.mp4")
         outfile = os.path.join(dir, 'hap', filename + '.mov')
-
-        start = get_video_start(infile)
-
-        resize_cmd = "ffmpeg -y -i {} -vf scale=w=1920:h=1080 -ss 00:00:0{} -c:v hap  -c:a aac -strict -2 -async 1 {}   ".format(
-            infile, start, outfile)
-
-        subprocess.call(resize_cmd, shell=True)
+        convert_resize(infile, outfile)
 
 
 if __name__ =="__main__":
     VIDEO_DIR = "/home/nuc/Documents/dataStore/VIDEO"
     files = find_oversize(VIDEO_DIR)
-    reencode_oversize(files)
+    print(files)
+    reencode_oversize(VIDEO_DIR,files)
